d7603470f9f4d87c08b447612483dc41dc8f4ddd
wesley <wesbourkeavl@gmail.com>
2022-04-22 13:59:47 -0400

    touch up for no reason.

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,13 @@ function Home() {
   return (
     <Box>
       <CrazyImageBox />
-      <Paper>
-        <p>What this company is all about? askdfjakdjaklsjka;lsfaskjajasdkfjaksdjfkl;adaskfjdaksjfalksdjfkjasdfkjafjasldjfajsfjaksfljads;kfjanmcasd   sakfjas j;lasfj ;lasj dfla;j flaj fj asfd ;laskfj kasl;dfj ;alskfdj la;sdj fasjk fl;ajsdflk; jasdfkl; jakls;d fjakl;sjdfklajds fk sajdkf jals;dfj laksdja s asdjf ;ldasj fka jsdfljasdfj aasjdfksdjfkajs dflk jadskf asjfaklsd fjklsadjfk j jfkasdj ;asdjfklasj dflkajsdfkjaskfj alksd jflvcjanjnejh h sdhaksd jfaks djfkajsd f;kadjsf ;klsajdfancm jkansjdcaewniwanc jarnfeuancajsdcasndrenvuancja,n</p>
+      <Paper sx={{backgroundColor: '#362706'}}>
+        <h1 style={{color: '#464E2E', padding: '1rem'}}>Rock climbing: from ancient practice to Olympic sport</h1>
+        <p style={{color: '#E9E5D6', padding: '1rem'}}>Since modern recreational climbing began in the late 19th century, breakthroughs derived from new technology and tactics have pushed it in myriad directions.<br/>
+
+        Today, as international competitors at the highest echelons of the sport look forward to climbing’s debut in the 2020 Tokyo Olympics, thousands of new enthusiasts have been introduced to the sport thanks to the burgeoning indoor climbing gym industry. Meanwhile, ever growing numbers of elite climber-explorers continue to travel to the far ends of the earth in search of “untouched stone.”<br/>
+        _______________________________________________________________________________________________
+        </p>
       </Paper>
     </Box>
   )
